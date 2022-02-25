2dbe09040db622dc00c1c3a8823f28ccc32a07ab
woohm402 <woohm404@gmail.com>
2022-02-25 20:55:15 +0900

    [atomic] implement layout

--- pages/example.tsx
+++ pages/example.tsx
@@ -1,7 +1,12 @@
 import { Example } from '../components/pages';
+import { Layout } from '../components/templates/layout/layout';
 
 const ExamplePage = () => {
-  return <Example />;
+  return (
+    <Layout>
+      <Example />
+    </Layout>
+  );
 };
 
 export default ExamplePage;
